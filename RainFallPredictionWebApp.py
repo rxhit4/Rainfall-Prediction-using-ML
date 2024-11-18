aa18b097e1d7f99c3dceda4f49d7b344e96e95a1
Rohit Kumar Sarkar <134861870+rxhit4@users.noreply.github.com>
2024-11-19 03:11:34 +0530

    RainfallPredictionWebApp.py

--- RainFallPredictionWebApp.py
+++ RainFallPredictionWebApp.py
@@ -4,59 +4,49 @@ Created on Tue Nov 19 00:54:13 2024
 
 @author: rohit
 """
-
-import numpy as np
 import pandas as pd
 import pickle
 import streamlit as st
 
+# LOADING THE SAVED PICKLE MODEL
+loaded_model = pickle.load(open("C:/Users/Rohit Btech/Desktop/DEPLOYML/trained_model.sav", 'rb'))
 
-#LOADING THE SAVED PICKLE MODEL
-loaded_model = pickle.load(open("trained_model.sav", 'rb'))
-
-#CREATING A FUNCTION FOR PREDICTION
-
+# CREATING A FUNCTION FOR PREDICTION
 def rainfall_prediction(input_data):
-    
-    
-    input_df = pd.DataFrame([input_data], columns = ['pressure', 'temparature', 'dewpoint', 'humidity', 'cloud', 'sunshine', 'winddirection', 'windspeed'])
+    input_df = pd.DataFrame([input_data], columns=['pressure', 'temparature', 'dewpoint', 'humidity', 'cloud', 'sunshine', 'winddirection', 'windspeed'])
     prediction = loaded_model.predict(input_df)
-    return'Prediction result:', "Rainfall is expected" if prediction == 1 else "Rainfall is NOT expected"
-    
-    
-    def main():
-        
-        #giving a title
-        st.title('Rainfall Prediction Web App')
-        
-        #Getting the input data from the user
-        pressure = st.text_input('Pressure')
-        temparature = st.text_input('Temperature')
-        dewpoint = st.text_input('Dewpoint')
-        humidity = st.text_input('Humidity')
-        cloud = st.text_input('Cloud')
-        sunshine = st.text_input('Sunshine')
-        winddirection = st.text_input('Wind Direction')
-        windspeed = st.text_input('Wind Speed')
-        
-        
-        #CODE FOR PREDICTION
-        Forecasting = ''
-        
-        #Creating a Button for Prediction
-        if st.button('Rainfall Prediction'):
-            Forecasting = rainfall_prediction([pressure, temparature, dewpoint, humidity, cloud, sunshine, winddirection, windspeed])
-            
-        st.success(Forecasting)
-        
-        
-        
-        
-        
-        
-    if __name__ == "__main__":
-          main()
-        
+    if prediction[0] == 1:
+        return 'Rainfall is Expected'
+    else:
+        return 'Rainfall is NOT Expected'
+
+
+def main():
+    # Giving a title
+    st.title('Rainfall Prediction Web App')
+
+    # Getting the input data from the user
+    pressure = st.text_input('Pressure')
+    temparature = st.text_input('Temperature')
+    dewpoint = st.text_input('Dewpoint')
+    humidity = st.text_input('Humidity')
+    cloud = st.text_input('Cloud')
+    sunshine = st.text_input('Sunshine')
+    winddirection = st.text_input('Wind Direction')
+    windspeed = st.text_input('Wind Speed')
+
+    # Code for prediction
+    if st.button('Rainfall Prediction'):
+        # Make sure all inputs are filled
+        if all([pressure, temparature, dewpoint, humidity, cloud, sunshine, winddirection, windspeed]):
+            input_data = [pressure, temparature, dewpoint, humidity, cloud, sunshine, winddirection, windspeed]
+            forecasting = rainfall_prediction(input_data)
+            st.success(forecasting)
+        else:
+            st.error("Please fill in all fields")
+
+if __name__ == "__main__":
+    main()
         
         
         
